bc238f5f5baeab53c989aa82789b5b3cec224ad6
cristhian9870 <cisian1234@hotmail.com>
2025-06-17 19:20:07 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,6 +3,49 @@ $(".a-c").on("click", function () {
   window.location.href = "./pages/bandeja.html";
 });
 
+<<<<<<< Updated upstream
+=======
+//Definición de variables
+var saludo_1 = "Hola Bienvenido";
+let saludo_2 = "Hola Bienvenido";
+const saludo_3 = "Hola Bienvenido";
+
+let edad = 25
+let es_noche = true;
+
+let computadora = {
+  ram: 64;
+  cpu "Rysen 9 s98xx"
+  sdd: 2048;
+  activo: true
+  precioventa: function(){
+    let precioparcial = this.ram * 50; 3200
+    let precioparcial = this.ssd >= 1024 ? 1000 : 500 ; 
+
+    return precioByRam + precioBySSD + 1500;
+  }
+}
+//Definición de una función
+var saludo_1 = "hola bienvenido"
+let saludo_2 = "hola bienvenido"
+const saludo_3 = "hola bienvenido"
+
+
+
+
+function saludar() {
+  let mensaje = "Hola Bienvenidos";
+  console.log(mensaje);
+}
+
+function saludarDos(mensaje) {
+  console.log(mensaje);
+}
+
+function saludarTres(nombre, edad) {
+  return "Hola " + nombre + ", tienes " + edad + " años";
+}
+>>>>>>> Stashed changes
 
 let nroIntentos = 0;
 function login(usuario, contrasenia) {
